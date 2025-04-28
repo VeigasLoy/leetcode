3c9fb2da3fa1ed27ff8e67877829d3f315e90c88
VeigasLoy <loystonv@gmail.com>
2025-04-28 10:35:45 +0530

    Create file

---
+++ 357-CountNumberswithUniqueDigits/357-CountNumberswithUniqueDigits.java
@@ -0,0 +1,14 @@
+// Last updated: 4/28/2025, 10:35:44 AM
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n==1) return 10;
+        if(n==2) return 91;
+        if(n==3) return 739;
+        if(n==4) return 5275;
+        if(n==5) return 32491;
+        if(n==6) return 168571;
+        if(n==7) return 712891;
+        if(n==8) return 2345851;
+        return 1;
+    }
+}
