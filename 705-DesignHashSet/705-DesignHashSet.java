d5a6ac523f3a3e2d29919d9537d0fd8da3ad6db5
DepLoy <loystonv@gmail.com>
2025-07-14 10:02:17 +0530

    Create 705-DesignHashSet

---
+++ 705-DesignHashSet/705-DesignHashSet.java
@@ -0,0 +1,27 @@
+// Last updated: 7/14/2025, 10:02:16 AM
+class MyHashSet {
+    HashSet<Integer> set;
+    public MyHashSet() {
+        set = new HashSet<>();
+    }
+    
+    public void add(int key) {
+        set.add(key);
+    }
+    
+    public void remove(int key) {
+        set.remove(key);
+    }
+    
+    public boolean contains(int key) {
+        return set.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
