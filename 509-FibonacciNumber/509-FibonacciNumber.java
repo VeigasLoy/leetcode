60ff600d3f4c3cfa111092c955c94a839a2d1aba
DepLoy <loystonv@gmail.com>
2025-07-17 10:13:14 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 7/17/2025, 10:13:14 AM
+class Solution {
+    int[] memo = new int[31];
+
+    public int fib(int n) {
+        if (n <= 1)
+            return n;
+        if (memo[n] != 0)
+            return memo[n];
+        memo[n] = fib(n - 1) + fib(n - 2);
+        return memo[n];
+    }
+}
