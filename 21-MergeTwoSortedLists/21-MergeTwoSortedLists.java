e774014083813f216280ee0ffd45fab6b7a13608
DepLoy <loystonv@gmail.com>
2025-07-24 21:17:53 +0530

    File updated at 7/24/2025, 9:17:52 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,32 +1,25 @@
-// Last updated: 7/24/2025, 8:44:03 PM
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// Last updated: 7/24/2025, 9:17:52 PM
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1==null && list2==null)  return null;
-        if(list1==null) return list2;
-        if(list2==null) return list1;
-        ListNode head = new ListNode(0);
-        ListNode merged = head;
-        while(list1!=null && list2!=null){
-            if(list1.val<list2.val){
-                merged.next = list1;
-                list1 = list1.next;
-            }   else{
-                        merged.next = list2;
-                        list2 = list2.next;
-            }
-            merged = merged.next;
+    public int removeDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        int i=0;
+        if(nums.length==1){
+            return 1;
         }
-        merged.next = (list1 != null) ? list1 : list2;
-        return head.next;
+        while(i<nums.length){
+            if(nums[i]!=nums[i+1]){
+                list.add(nums[i]);
+            }
+            i++;
+            if(i==nums.length-1){
+                list.add(nums[i]);
+                i++;
+            }
+                
+        }
+        for (i = 0; i < list.size(); i++) {
+            nums[i] = list.get(i);
+        }
+        return list.size();
     }
 }
