a7ab8dedd33cdeb221b81245b87fd40f4f417e1a
DepLoy <loystonv@gmail.com>
2025-07-24 20:44:04 +0530

    File updated at 7/24/2025, 8:44:03 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -1,4 +1,4 @@
-// Last updated: 7/24/2025, 8:41:54 PM
+// Last updated: 7/24/2025, 8:44:03 PM
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -18,24 +18,15 @@ class Solution {
         ListNode merged = head;
         while(list1!=null && list2!=null){
             if(list1.val<list2.val){
-                merged.next = new ListNode(list1.val);
+                merged.next = list1;
                 list1 = list1.next;
             }   else{
-                        merged.next = new ListNode(list2.val);
+                        merged.next = list2;
                         list2 = list2.next;
             }
             merged = merged.next;
         }
-        while(list1!=null){
-            merged.next = new ListNode(list1.val);
-            list1 = list1.next;
-            merged = merged.next;
-        }
-        while(list2!=null){
-            merged.next = new ListNode(list2.val);
-            list2 = list2.next;
-            merged = merged.next;
-        }
+        merged.next = (list1 != null) ? list1 : list2;
         return head.next;
     }
 }
