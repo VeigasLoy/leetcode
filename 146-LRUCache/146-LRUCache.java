ceb3cf004cb6dc5fb7ed987bd212221340558c74
DepLoy <loystonv@gmail.com>
2025-07-25 15:16:51 +0530

    Create 146-LRUCache

---
+++ 146-LRUCache/146-LRUCache.java
@@ -0,0 +1,26 @@
+// Last updated: 7/25/2025, 3:16:50 PM
+class LRUCache {
+    LinkedHashMap<Integer,Integer> map;
+    public LRUCache(int capacity) {
+        map = new LinkedHashMap<>(capacity, 0.75f, true){
+            protected boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest){
+                return size()>capacity;
+            }
+        };
+    }
+    
+    public int get(int key) {
+        return map.getOrDefault(key,-1);
+    }
+    
+    public void put(int key, int value) {
+        map.put(key,value);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
