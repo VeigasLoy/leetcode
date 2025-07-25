3b0d2f58070d40c1f3ab310c00490d0db94f3b77
DepLoy <loystonv@gmail.com>
2025-07-25 21:52:25 +0530

    File updated at 7/25/2025, 9:52:25 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,23 +1,21 @@
-// Last updated: 7/25/2025, 9:34:23 PM
+// Last updated: 7/25/2025, 9:52:25 PM
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int pre[] = new int[n];
-        int suff[] = new int[n];
-        pre[0] = 1;
-        suff[n - 1] = 1;
-        
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i - 1];
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()==0||s.length()==1)   return s.length();
+        int start = 0, end = 1;
+        int len = 0;
+        ArrayList<Character> al = new ArrayList<>();
+        al.add(s.charAt(0)); 
+        while(end<s.length()){
+            if(al.contains(s.charAt(end))){
+                al.subList(0,al.indexOf(s.charAt(end))+1).clear();
+            }
+            if(!al.contains(s.charAt(end))){
+                al.add(s.charAt(end));
+            }
+            len = Math.max(len,al.size());
+            end++;
         }
-        for(int i = n - 2; i >= 0; i--) {
-            suff[i] = suff[i + 1] * nums[i + 1];
-        }
-        
-        int ans[] = new int[n];
-        for(int i = 0; i < n; i++) {
-            ans[i] = pre[i] * suff[i];
-        }
-        return ans;
+        return len;
     }
 }
