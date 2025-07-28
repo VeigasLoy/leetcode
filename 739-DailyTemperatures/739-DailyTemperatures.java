d92a8f78874f77744c70488f835574e4f65f0224
DepLoy <loystonv@gmail.com>
2025-07-28 12:37:55 +0530

    Create 739-DailyTemperatures

---
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -0,0 +1,10 @@
+// Last updated: 7/28/2025, 12:37:55 PM
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j=0;
+        for(int i=m;i<nums1.length;i++)
+            nums1[i] = nums2[j++];
+
+        Arrays.sort(nums1);
+    }
+}
