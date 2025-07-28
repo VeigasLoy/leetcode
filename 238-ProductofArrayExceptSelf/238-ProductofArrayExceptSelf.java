6d65a385f281a36b86807cf8e86daa2e79eba1f3
DepLoy <loystonv@gmail.com>
2025-07-28 10:02:46 +0530

    File updated at 7/28/2025, 10:02:46 AM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,10 +1,11 @@
-// Last updated: 7/28/2025, 9:58:30 AM
+// Last updated: 7/28/2025, 10:02:46 AM
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
         int left = 1, right = 1;
-        Arrays.fill(res,1);
+        for(int i=0;i<n;i++)
+            res[i]=1;
         for(int i=0;i<n;i++){
             res[i] *= left;
             left *= nums[i];
