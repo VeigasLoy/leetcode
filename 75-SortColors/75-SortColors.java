26f4aa2c6124a4a3411c7461380450ea3e49b494
DepLoy <loystonv@gmail.com>
2025-07-28 16:00:35 +0530

    File updated at 7/28/2025, 4:00:35 PM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,12 +1,22 @@
-// Last updated: 7/28/2025, 3:21:55 PM
+// Last updated: 7/28/2025, 4:00:35 PM
 class Solution {
-    public void reverseString(char[] s) {
-        int j=s.length-1;
-        int i=0;
-        while(i<j){
-            char temp = s[i];
-            s[i++] = s[j];
-            s[j--] = temp;        
+    public String reverseStr(String s, int k) {
+        char ch[]=s.toCharArray();
+        int n=ch.length;
+        for (int st=0;st<n;st+=2*k){
+            int i=st;
+            int j;
+            if(st+k-1<n)
+                j=st+k-1;
+            else
+                j=n-1;
+   
+            while(i<j){
+                char t=ch[i];
+                ch[i++]=ch[j];
+                ch[j--]=t;
+            }
         }
+        return String.valueOf(ch);
     }
 }
