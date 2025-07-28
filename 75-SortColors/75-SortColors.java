3fe6dcf60d21a52e7b15903d79c9a2a73674556e
DepLoy <loystonv@gmail.com>
2025-07-28 14:51:18 +0530

    File updated at 7/28/2025, 2:51:18 PM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,19 +1,25 @@
-// Last updated: 7/28/2025, 2:31:48 PM
+// Last updated: 7/28/2025, 2:51:18 PM
 class Solution {
     public void sortColors(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=nums.length-1;i<j;j--){
-                if(nums[i]==2 && nums[i]>nums[j]){
-                    int temp = nums[j];
-                    nums[j]=nums[i];
-                    nums[i]=temp;
-                }
-                else if(nums[i]==1 && nums[j]==0){
-                    int temp = nums[j];
-                    nums[j]=nums[i];
-                    nums[i]=temp;
-                }    
+        int low = 0, high = nums.length - 1;
+        int i = 0;
+        while(i <= high){
+            if (nums[i] == 0) {
+                swap(nums, i, low);
+                low++;
+                i++;
+            } else if(nums[i] == 2){
+                swap(nums, i, high);
+                high--;
+            } else{
+                i++;
             }
         }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
