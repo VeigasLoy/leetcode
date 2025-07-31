9431479afffdb9fad185f22a1847383782ad0c9f
DepLoy <loystonv@gmail.com>
2025-07-31 21:18:46 +0530

    Create 560-SubarraySumEqualsK

---
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -0,0 +1,20 @@
+// Last updated: 7/31/2025, 9:18:45 PM
+class Solution {
+    public String decToBin(int n){
+        StringBuilder s = new StringBuilder();
+        while(n>0){
+            s.append(n%2);
+            n/=2;
+        }
+        while(s.length()<32){
+            s.append(0);
+        }
+        System.out.println(s);
+        // System.out.println(s.length());
+        return s.toString();
+    }
+    public int reverseBits(int n) {
+        String s = decToBin(n);
+        return Integer.parseInt(s,2);
+    }
+}
