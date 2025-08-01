afe1cabaa926eb32f7a421630d46e36b5b06f30f
DepLoy <loystonv@gmail.com>
2025-08-01 09:50:37 +0530

    File updated at 8/1/2025, 9:50:37 AM

--- 2000-ReversePrefixofWord/2000-ReversePrefixofWord.java
+++ 2000-ReversePrefixofWord/2000-ReversePrefixofWord.java
@@ -1,12 +1,26 @@
-// Last updated: 8/1/2025, 9:37:59 AM
+// Last updated: 8/1/2025, 9:50:37 AM
 class Solution {
-    public String reversePrefix(String word, char ch) {
-        int idx = word.indexOf(ch);
-        if(idx==(-1))   return word;
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        sb2.append(sb1.append(word.substring(0,idx+1)).reverse());
-        sb2.append(word.substring(idx+1));
-        return sb2.toString();
+    public boolean isVowel(char c){
+        if((c=='a')||(c=='A')) return true;
+        if((c=='e')||(c=='E')) return true;
+        if((c=='i')||(c=='I')) return true;
+        if((c=='o')||(c=='O')) return true;
+        if((c=='u')||(c=='U')) return true;
+        return false;
+    }
+    public String reverseVowels(String s) {
+        char[] ch = s.toCharArray();
+        int start=0, end = ch.length-1;
+        while(start<end){
+            if(isVowel(ch[start]) && isVowel(ch[end])){
+                char tmp = ch[start];
+                ch[start++] = ch[end];
+                ch[end--] = tmp;
+            }
+            else if(!isVowel(ch[start]))    start++;
+            else    end--;
+        }
+        StringBuilder sb = new StringBuilder(String.valueOf(ch));
+        return sb.toString(); 
     }
 }
