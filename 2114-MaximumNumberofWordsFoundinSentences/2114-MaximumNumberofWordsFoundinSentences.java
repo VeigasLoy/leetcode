e4f7132fb709528229239b9a865caf9788b6cc78
DepLoy <loystonv@gmail.com>
2025-08-01 20:24:24 +0530

    File updated at 8/1/2025, 8:24:24 PM

--- 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.java
+++ 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.java
@@ -1,10 +1,14 @@
-// Last updated: 8/1/2025, 8:22:25 PM
+// Last updated: 8/1/2025, 8:24:24 PM
 class Solution {
     public int mostWordsFound(String[] sentences) {
+        boolean isword=false;
         int max=0;
         for(int i=0;i<sentences.length;i++){
-            String[] words = sentences[i].split("\\s");
-            max = Math.max(max,words.length);
+            String[] str=sentences[i].split(" ");
+            int count=str.length;
+            if(count>max){
+                max = count;
+            }
         }
         return max;
     }
